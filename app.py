f47388423f4f0d1bde3180f10db0f0041bad7cb2
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-10-02 16:00:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ with st.sidebar:
   )
   
   stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
-  stroke_color = '#FFFFFF' # Set background color '#000000'
+  stroke_color = st.color_picker("Elige un Color", "#FFFFFF")
   bg_color = '#000000'
 
 # Create a canvas component
