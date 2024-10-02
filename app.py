1711503b04373400e013f891dd773c6742f9b334
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-10-02 14:48:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+
+
+drawing_mode = "freedraw"
+stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+stroke_color = '#FFFFFF' # Set background color 
+bg_color = '#000000'
+
+# Create a canvas component
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    height=200,
+    width=200,
+    key="canvas",
+)
