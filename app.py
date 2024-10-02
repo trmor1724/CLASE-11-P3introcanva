4de83d791559d51cd480faadfedfd0b044074f5a
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-10-02 15:54:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ canvas_result = st_canvas(
     stroke_width=stroke_width,
     stroke_color=stroke_color,
     background_color=bg_color,
-    height=200,
-    width=200,
+    height=400,
+    width=300,
     drawing_mode=drawing_mode,
     key="canvas",
 )
